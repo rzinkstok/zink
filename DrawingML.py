3f8cd326ad200ad3b25a0cc4dd1437a384166a22
Roel Zinkstok <rzinkstok@gmail.com>
2018-01-13 10:37:14 +0100

    Adds license

--- DrawingML.py
+++ DrawingML.py
@@ -1,4 +1,20 @@
-###!/Library/Frameworks/Python.framework/Versions/3.1/bin/python
+#    Copyright 2018 Roel Zinkstok
+#
+#    This file is part of zink.
+#
+#    zink is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    zink is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with zink.  If not, see <http://www.gnu.org/licenses/>.
+
 
 import docxBase
 
